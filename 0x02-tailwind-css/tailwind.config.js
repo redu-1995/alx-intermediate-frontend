03ee35e4278f8a2e913d953d8c22d59ba89947ef
redu-1995 <nara1415karol@gmail.com>
2025-05-25 17:43:50 +0300

    Add grid and flexbox layout + image gallery

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./1-index.html",  
+     "./2-index.html",
+     "./3-nav_index.html" ,  
+                // for root index.html
+    "./src/**/*.{html,js}", // for all html/js in src folder and subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
